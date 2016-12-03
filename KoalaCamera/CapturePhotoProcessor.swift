cc6569ed2fd65d1029fc2ddcc39bac475646f394
Edward Kim <onward.edkim@gmail.com>
2016-12-03 21:25:03 +1100

    Remove unused code

--- KoalaCamera/CapturePhotoProcessor.swift
+++ KoalaCamera/CapturePhotoProcessor.swift
@@ -224,18 +224,3 @@ extension UIDevice {
     }
     
 }
-
-typealias Rational = (num : Int, den : Int)
-
-func rationalApproximation(of x0 : Double, withPrecision eps : Double = 1.0E-6) -> Rational {
-    var x = x0
-    var a = x.rounded(.down)
-    var (h1, k1, h, k) = (1, 0, Int(a), 1)
-    
-    while x - a > eps * Double(k) * Double(k) {
-        x = 1.0/(x - a)
-        a = x.rounded(.down)
-        (h1, k1, h, k) = (h, k, h1 + Int(a) * h, k1 + Int(a) * k)
-    }
-    return (h, k)
-}
